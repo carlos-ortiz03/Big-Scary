09c019e727d9dae603319df38e40f7f067996af1
elliecox02 <elizabethccox02@gmail.com>
2020-02-07 13:34:40 -0600

    Button scripts

---
+++ Assets/Scripts/restartButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class restartButton : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
