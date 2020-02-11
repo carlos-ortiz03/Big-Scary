40aa531f766b6c95d31abc247a275a644eb534c9
Nagishiro <sagalilyace@gmail.com>
2020-02-11 14:34:34 -0600

    transition is torture WIP

---
+++ Assets/Scripts/Animation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+
+{
+    public CharacterController controller;
+    private Animator _animator;
+
+    void start()
+    {
+        controller = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+    }
+    public Animator animate;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) ;
+        {
+            _animator.SetInteger("Condition", 1);
+            Debug.Log("It works");
+        }
+    }
+}
