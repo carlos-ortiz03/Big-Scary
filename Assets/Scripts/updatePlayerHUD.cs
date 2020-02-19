0520d589a677c3b327fe3f34e9e2244d772a49e5
Nagishiro <sagalilyace@gmail.com>
2020-02-19 14:00:31 -0600

    Merge branch 'master' of https://github.com/carlosxcortizis1/Big-Scary
    # Conflicts:
    #	Assets/Scenes/ScaryGame.unity
    #	Assets/Scripts/PlayerMovementScript.cs

---
+++ Assets/Scripts/updatePlayerHUD.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class updatePlayerHUD : MonoBehaviour
+{
+    
+    private TextMeshPro playerHUD;
+
+    private GameObject camera;
+    
+    private Boolean key1Stat;
+    private Boolean key2Stat;
+
+    void Start()
+    {
+        camera = GameObject.Find("PLAYER/Main Camera");
+        playerHUD = GetComponent<TextMeshPro>();
+    }
+    
+    void Update()
+    {
+        clickToDelete test = camera.GetComponent<clickToDelete>();
+        
+        key1Stat = test.key1;
+        key2Stat = test.key2;
+        
+        updateHUD();
+    }
+    
+    private void updateHUD()
+    {
+        if (key1Stat == false && key2Stat == false) playerHUD.text = "Gold Key: Lost \nSilver Key: Lost";
+        if (key1Stat == true && key2Stat == false) playerHUD.text = "Gold Key: Found \nSilver Key: Lost";
+        if (key1Stat == false && key2Stat == true) playerHUD.text = "Gold Key: Lost \nSilver Key: Found";
+        if (key1Stat == true && key2Stat == true) playerHUD.text = "Gold Key: Found \nSilver Key: Found";
+    }
+}
