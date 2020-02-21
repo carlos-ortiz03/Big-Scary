c6a7166957a29d1e49e223c8f2c5a792efeb8301
elliecox02 <elizabethccox02@gmail.com>
2020-02-21 12:24:52 -0600

    updated button scene

---
+++ Assets/Scripts/mainMenuButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenuButton : MonoBehaviour
+{
+    void Start()
+    {
+        void mainMenu()
+        {
+            // SceneManager.LoadScene(SceneManager.GetActiveScene().mainMenu;
+        }
+    }
+}
--- Assets/Scripts/restartButton.cs
+++ Assets/Scripts/restartButton.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class restartButton : MonoBehaviour
 {
     public void Restart()
     {
-        
+        // SceneManager.LoadScene)SceneManager.GetActiveScene().name);
     }
 }
