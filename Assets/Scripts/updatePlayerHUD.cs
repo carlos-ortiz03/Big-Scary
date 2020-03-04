39ae02aebf49cb0615f87776a2c560528096cf8a
TheVietVu <60711148+TheVietVu@users.noreply.github.com>
2020-03-04 12:46:43 -0600

    Just some edits and polishing

--- Assets/Scripts/updatePlayerHUD.cs
+++ Assets/Scripts/updatePlayerHUD.cs
@@ -9,10 +9,9 @@ public class updatePlayerHUD : MonoBehaviour
 
     private GameObject camera;
 
-    private GameObject i1;
-    private GameObject i2;
-    private GameObject i3;
-    private GameObject i4;
+    private GameObject key1Obj;
+    private GameObject key2Obj;
+    private GameObject inventory;
     
     private Boolean key1Stat;
     private Boolean key2Stat;
@@ -22,10 +21,9 @@ public class updatePlayerHUD : MonoBehaviour
     void Start()
     {
         camera = GameObject.FindWithTag("MainCamera");
-        i1 = GameObject.FindWithTag("I1");
-        i2 = GameObject.FindWithTag("I2");
-        i3 = GameObject.FindWithTag("I3");
-        i4 = GameObject.FindWithTag("I4");
+        key1Obj = GameObject.FindWithTag("Key1Obj");
+        key2Obj = GameObject.FindWithTag("Key2Obj");
+        inventory = GameObject.FindWithTag("Inventory");
         isOpen = false;
     }
 
@@ -37,57 +35,14 @@ public class updatePlayerHUD : MonoBehaviour
         key2Stat = test.key2;
 
         if (Input.GetKeyDown(KeyCode.Tab)) isOpen = !isOpen;
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            Debug.Log("isOpen: " + isOpen);
-        }
 
         updateHUD();
     }
 
     private void updateHUD()
     {
-        if (isOpen == true)
-        {
-            if (key1Stat == false && key2Stat == false)
-            {
-                i1.SetActive(true);
-                i2.SetActive(false);
-                i3.SetActive(false);
-                i4.SetActive(false);
-                Debug.Log("i1");
-            }
-            if (key1Stat == false && key2Stat == true)
-            {
-                i1.SetActive(false);
-                i2.SetActive(true);
-                i3.SetActive(false);
-                i4.SetActive(false);
-                Debug.Log("i2");
-            }
-            if (key1Stat == true && key2Stat == false)
-            {
-                i1.SetActive(false);
-                i2.SetActive(false);
-                i3.SetActive(true);
-                i4.SetActive(false);
-                Debug.Log("i3");
-            }
-            if (key1Stat == true && key2Stat == true)
-            {
-                i1.SetActive(false);
-                i2.SetActive(false);
-                i3.SetActive(false);
-                i4.SetActive(true);
-                Debug.Log("i4");
-            }
-        }
-        if (isOpen == false)
-        {
-            i1.SetActive(false);
-            i2.SetActive(false);
-            i3.SetActive(false);
-            i4.SetActive(false);
-        }
+        inventory.SetActive(isOpen);
+        key1Obj.SetActive(key1Stat);
+        key2Obj.SetActive(key2Stat);
     }
 }
